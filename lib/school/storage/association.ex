02e2defea7c1ff5a365d1f5d3d51de02cfb03138
Zack Siri <zacksiri@gmail.com>
2021-09-29 17:10:16 +0700

    Added association

---
+++ lib/school/storage/association.ex
@@ -0,0 +1,11 @@
+defmodule School.Storage.Association do
+  @derive Jason.Encoder
+  defstruct [:resource_id, :module]
+
+  def new(%m{id: resource_id}) do
+    %__MODULE__{
+      resource_id: resource_id,
+      module: m
+    }
+  end
+end
